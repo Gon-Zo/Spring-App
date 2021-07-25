90fc78ce5499539441317a4fd29eca2b9c82ad59
‘gonzo’ <pnw123456@icloud.com>
2021-07-25 09:10:38 +0900

    fix: class name update

--- jpa/src/main/java/io/gonzo/jpa/app/repository/support/SampleRepositoySupport.java
+++ jpa/src/main/java/io/gonzo/jpa/app/repository/support/SampleRepositoySupport.java
@@ -1,20 +1,21 @@
-package io.gonzo.jpa.app.querydsl;
+package io.gonzo.jpa.app.repository.support;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.gonzo.jpa.app.domain.basic.Customers;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
 @Repository
-public class SampleRepositoySuport extends QuerydslRepositorySupport {
+public class SampleRepositoySupport extends QuerydslRepositorySupport {
 
     private final JPAQueryFactory jpaQueryFactory;
 
     private final EntityManager entityManager;
 
-    public SampleRepositoySuport(Class<?> domainClass, JPAQueryFactory jpaQueryFactory, EntityManager entityManager) {
-        super(domainClass);
+    public SampleRepositoySupport(JPAQueryFactory jpaQueryFactory, EntityManager entityManager) {
+        super(Customers.class);
         this.jpaQueryFactory = jpaQueryFactory;
         this.entityManager = entityManager;
     }
