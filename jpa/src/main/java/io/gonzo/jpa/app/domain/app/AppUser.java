c80bf8200cf18a813a0cc435498af46330233fdd
‘gonzo’ <pnw123456@icloud.com>
2021-08-04 19:41:54 +0900

    refactor: app_user entity update

--- jpa/src/main/java/io/gonzo/jpa/app/domain/app/AppUser.java
+++ jpa/src/main/java/io/gonzo/jpa/app/domain/app/AppUser.java
@@ -4,16 +4,20 @@ import io.gonzo.jpa.app.domain.app.base.AppBaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
 @Getter
+@DynamicInsert
+@DynamicUpdate
 @NoArgsConstructor
 @Table(name = "app_user")
 public class AppUser extends AppBaseEntity {
@@ -49,6 +53,31 @@ public class AppUser extends AppBaseEntity {
         this.gender = gender;
         this.count = count;
     }
+
+    @PrePersist
+    public void prePersist() {
+
+        if (StringUtils.isEmpty(this.firstName)) {
+            this.firstName = null;
+        }
+
+        if (StringUtils.isEmpty(this.lastName)) {
+            this.lastName = null;
+        }
+
+        if (StringUtils.isEmpty(this.email)) {
+            this.email = null;
+        }
+
+        if (StringUtils.isEmpty(this.gender)) {
+            this.gender = null;
+        }
+
+        if (ObjectUtils.isEmpty(this.count)) {
+            this.count = null;
+        }
+    }
+
 
 }
 
