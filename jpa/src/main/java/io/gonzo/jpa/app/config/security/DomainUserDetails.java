9d2e48a720622ddc24cf7cea6aaddaf430b13962
‘gonzo’ <pnw123456@icloud.com>
2021-08-26 22:25:02 +0900

    chore: remark remove

--- jpa/src/main/java/io/gonzo/jpa/app/config/security/DomainUserDetails.java
+++ jpa/src/main/java/io/gonzo/jpa/app/config/security/DomainUserDetails.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
-//import org.springframework.security.core.userdetails.UserDetails;
 
 public class DomainUserDetails extends User {
 
