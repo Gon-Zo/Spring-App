c60ad61dcb261f080986db361ddb130650d5bef9
‘gonzo’ <pnw123456@icloud.com>
2021-08-28 12:02:38 +0900

    feat: jpa dynamic select to dto

--- jpa/src/main/java/io/gonzo/jpa/app/repository/AuthRepository.java
+++ jpa/src/main/java/io/gonzo/jpa/app/repository/AuthRepository.java
@@ -4,10 +4,13 @@ import io.gonzo.jpa.app.domain.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface AuthRepository extends JpaRepository<Auth, Long> {
     @Override
     List<Auth> findAll();
+
+    <T> Collection<T> findByAuthName(String authName, Class<T> type);
 }
--- jpa/src/main/java/io/gonzo/jpa/app/web/AuthResource.java
+++ jpa/src/main/java/io/gonzo/jpa/app/web/AuthResource.java
@@ -2,11 +2,14 @@ package io.gonzo.jpa.app.web;
 
 import io.gonzo.jpa.app.domain.Auth;
 import io.gonzo.jpa.app.repository.AuthRepository;
+import io.gonzo.jpa.app.web.dto.AuthDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -19,6 +22,11 @@ public class AuthResource {
     @GetMapping("")
     public List<Auth> showAuthList() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{authName}")
+    public Collection<AuthDTO> showAuthList(@PathVariable String authName) {
+        return repository.findByAuthName(authName, AuthDTO.class);
     }
 
 }
