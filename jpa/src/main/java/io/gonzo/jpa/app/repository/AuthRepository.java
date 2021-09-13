95cb77b83dd8d4c0b3ee2c182d0698ddfaffec75
‘gonzo’ <pnw123456@icloud.com>
2021-09-13 22:46:51 +0900

    feat: Declaring manipulating queries
    - use to update query

--- jpa/src/main/java/io/gonzo/jpa/app/repository/AuthRepository.java
+++ jpa/src/main/java/io/gonzo/jpa/app/repository/AuthRepository.java
@@ -2,6 +2,9 @@ package io.gonzo.jpa.app.repository;
 
 import io.gonzo.jpa.app.domain.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -13,4 +16,9 @@ public interface AuthRepository extends JpaRepository<Auth, Long> {
     List<Auth> findAll();
 
     <T> Collection<T> findByAuthName(String authName, Class<T> type);
+
+    @Modifying
+    @Query("update Auth auth set auth.authName = :authName where auth.id = :id")
+    long setFixedAuthNameFor(@Param("authName") String authName, @Param("id") Long id);
+    
 }
