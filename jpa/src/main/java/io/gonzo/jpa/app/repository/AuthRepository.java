bcd0aaf6a88e35565c1f1bd89bf57c09fb036205
‘gonzo’ <pnw123456@icloud.com>
2021-09-14 22:05:12 +0900

    feat: use to @Query / JPQL

--- jpa/src/main/java/io/gonzo/jpa/app/repository/AuthRepository.java
+++ jpa/src/main/java/io/gonzo/jpa/app/repository/AuthRepository.java
@@ -1,6 +1,7 @@
 package io.gonzo.jpa.app.repository;
 
 import io.gonzo.jpa.app.domain.Auth;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AuthRepository extends JpaRepository<Auth, Long> {
@@ -24,5 +26,15 @@ public interface AuthRepository extends JpaRepository<Auth, Long> {
     @Modifying
     @Query("delete from Auth auth  where auth.authName = :authName")
     void deleteByAuthName(@Param("authName") String authName);
+
+    @Override
+    @Query("select auth from Auth auth where auth.id =: id")
+    Optional<Auth> findById(@Param("id") Long id);
+
+    @Query(value = "select auth from Auth auth where auth.authName like %?1", nativeQuery = true)
+    List<Auth> findByAuthNameEndsWith(String authName);
+
+    @Query("select auth from Auth auth where auth.authName =: authName")
+    List<Auth> findByAuthName(String authName, JpaSort sort);
 
 }
