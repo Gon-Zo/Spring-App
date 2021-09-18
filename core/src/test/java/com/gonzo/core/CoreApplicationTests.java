3463fbd2f95e67c54b9978b0b32a8a87f1bf972f
‘gonzo’ <pnw123456@icloud.com>
2021-09-18 11:34:22 +0900

    test: di constructor example test case

--- core/src/test/java/com/gonzo/core/CoreApplicationTests.java
+++ core/src/test/java/com/gonzo/core/CoreApplicationTests.java
@@ -1,10 +1,27 @@
 package com.gonzo.core;
 
+import com.gonzo.core.app.configuration.ExConfiguration;
+import com.gonzo.core.app.entity.Store;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
+
+    @Autowired
+    ExConfiguration exConfiguration;
+
+    @Test
+    @DisplayName("DI Example1")
+    void loadByExConfiguration() {
+        Store store = exConfiguration.getStore();
+        String itemTitle = store.getItem().getTitle();
+        Assertions.assertEquals(itemTitle, "사과나무");
+        return;
+    }
 
     @Test
     void contextLoads() {
